c1dfd84bb41db39af8b714d6970d009acebd2db2
kirucogeramger <klemu@byupathway.edu>
2025-03-06 00:17:41 +0300

    list sum and average

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -188,6 +188,27 @@ int numberRandom = randomGenerator.Next(1, 11);
 
 
 
+List<int> numbers = new List<int>();
+List<string> words = new List<string>();
+
+
+words.Add("phone");
+words.Add("keyboard");
+words.Add("mouse");
+
+Console.WriteLine(words.Count);
+
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+
+for (int i = 0; i < words.Count; i++)
+{
+    Console.WriteLine(words[i]);
+}
 
 
 
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers one after another when you finish enter 0.");
+        List<double> numbers = new List<double>();
+        int sum = 0;
+        int number = 1;
+        while (number != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+
+            sum += number;
+        }
+        int count = numbers.Count - 1;
+        foreach (double num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine($"You entered {count} numbers.");
+        Console.WriteLine($"The sum of the numbers is {sum}.");
+        Console.WriteLine($"The average of the numbers is {sum / count}.");
+
     }
 }
