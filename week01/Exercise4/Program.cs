fe01ba3d018da40b6d285631f889166faad4e19e
kirucogeramger <klemu@byupathway.edu>
2025-03-06 00:48:29 +0300

    streach chalange sort and smalest positive number

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -12,18 +12,43 @@ class Program
         {
             Console.WriteLine("Enter a number: ");
             number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                break;
+            }
+            else
+            {
+            
             numbers.Add(number);
 
             sum += number;
+            }
         }
-        int count = numbers.Count - 1;
+        int count = numbers.Count;
+        
+        Console.WriteLine($"You entered {count} numbers.");
+        Console.WriteLine($"The sum of the numbers is {sum}.");
+        Console.WriteLine($"The average of the numbers is {sum / count}.");
+        Console.WriteLine($"The largest number is {numbers.Max()}.");
+        Console.WriteLine($"The smallest number is {numbers.Min()}.");
+        Console.WriteLine("the sorted numbers in Ascending order are: ");
+        numbers.Sort();
+        //numbers.Reverse();
         foreach (double num in numbers)
         {
             Console.WriteLine(num);
         }
-        Console.WriteLine($"You entered {count} numbers.");
-        Console.WriteLine($"The sum of the numbers is {sum}.");
-        Console.WriteLine($"The average of the numbers is {sum / count}.");
+        Console.WriteLine("the positive number closest to zero is: ");
+        double closestToZero = numbers[0];
+        foreach (double num in numbers)
+        {
+            if (num > 0 && num < closestToZero)
+            {
+                closestToZero = num;
+            }
+        }
+        Console.WriteLine(closestToZero);
+
 
     }
 }
