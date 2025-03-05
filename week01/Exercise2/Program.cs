26572aa8a57c5a7d23f2489ab70ceb5b21c0d75b
kirucogeramger <klemu@byupathway.edu>
2025-03-05 15:32:01 +0300

    Grade signe are added

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,9 +5,28 @@ class Program
     static void Main(string[] args)
     {
         string gradeLater = "";
+        string gradeSaign = "";
+        float gradeRemainder = 0;
         Console.Write("What is your grade? ");
-        int grade = int.Parse(Console.ReadLine());
+        float grade = float.Parse(Console.ReadLine());
+        // the following code will determine the grade sign
+        gradeRemainder = grade % 10;
+        
+        if (gradeRemainder >= 7)
+        {
+            gradeSaign = "+";
+        }
+        else if (gradeRemainder < 3)
+        {
+            gradeSaign = "-";
+        }
+        else
+        {
+            gradeSaign = "";
+        }  
+        
 
+        // the following code will determine if the student passed or failed
             if (grade >= 70)
             {
                 Console.WriteLine("Congradulations You passed.");
@@ -17,7 +36,7 @@ class Program
                 Console.WriteLine("We are sorry You failed, please try again.");
             } 
             
-
+            // the following code will determine the grade letter
                 if (grade >= 90)
                 {
                     gradeLater = "A";
@@ -38,7 +57,27 @@ class Program
                 {
                     gradeLater = "F";
                 }
+            // the following code will print the grade with the sign  excluding the grade of "F" and "A+"
+            if (gradeLater == "F")
+            {   
                 Console.WriteLine($"You Got {gradeLater}.");
+            }
+            else if (gradeLater == "A")
+            {
+                if (gradeSaign == "-")
+                {
+                    Console.WriteLine($"You Got {gradeLater}{gradeSaign}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You Got {gradeLater}.");
+                }
+                
+            }
+            else 
+            {
+                Console.WriteLine($"You Got {gradeLater}{gradeSaign}.");
+            }
         
     }
 }
